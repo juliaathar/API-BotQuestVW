247c9706c2c3bf947f15381830066a60cf4d18cd
juliaathar <47447272809@info.local>
2023-10-31 20:35:22 -0300

    feito model do usuário

--- apibotquestvw/src/main/java/com/senai/apibotquestvw/models/UsuarioModel.java
+++ apibotquestvw/src/main/java/com/senai/apibotquestvw/models/UsuarioModel.java
@@ -4,16 +4,31 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+import java.util.Date;
 import java.util.UUID;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "usuario_model")
-public class UsuarioModel {
+@Table(name = "tb_usuario")
+public class UsuarioModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private UUID id;
+
+    private int tipo_usuario;
+    private String nome;
+    private String email;
+    private String senha;
+    private int vwId;
+    private Date dataNascimento;
+
+    @ManyToOne
+    @JoinColumn(name = "id_chapa", referencedColumnName = "id")
+    private UsuarioModel chapa;
 
 }
